e1757ea4bdfec3bc7241fe615254066454312dcc
IGORBIANCHINISPAGNOL <bianchinispagnolIgor@gmail.com>
2022-04-03 16:36:24 -0300

    alterando-connectionstring

--- SAFFARI/DataBase/Connection.cs
+++ SAFFARI/DataBase/Connection.cs
@@ -8,13 +8,13 @@ namespace DataBase
 {
     public class Connection
     {
-        private static string ConnectionLounge = @"Data Source=DESKTOP-KQ2HLDN\SQLEXPRESS; Integrated Security=False;Initial Catalog=SAFFARI;User ID=LOUNGE;Password=31649782;Connect Timeout=200";
-        //private static string ConnectionLocal = @"Data Source=IGOR\SQLEXPRESS134679; Integrated Security=True;Initial Catalog=SAFFARI";
+        //private static string ConnectionLounge = @"Data Source=DESKTOP-KQ2HLDN\SQLEXPRESS; Integrated Security=False;Initial Catalog=SAFFARI;User ID=LOUNGE;Password=31649782;Connect Timeout=200";
+        private static string ConnectionLocal = @"Data Source=IGOR\SQLEXPRESS134679; Integrated Security=True;Initial Catalog=SAFFARI";
 
 
         public static string StrCon
         {
-            get { return ConnectionLounge; }
+            get { return ConnectionLocal; }
         }
     }
 }
